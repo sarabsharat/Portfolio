b11428680a30552d3e43d7d83f7583bef8545e12
sarabsharat <sarabsharat.sb@gmail.com>
2025-02-14 23:13:38 +0300

    dude

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { AnimationMixer } from 'three';
 
 
-const scene = new THREE.Scene();
+// scene set up for web
 const canvas = document.getElementById("experience-canvas");
 const sizes = {
   width: window.innerWidth,
@@ -16,30 +16,37 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias:true});
 renderer.setSize( sizes.width, sizes.height );
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.outputEncoding = THREE.sRGBEncoding;
-THREE.ColorManagement.enabled = true;
+const scene = new THREE.Scene();
+{
+  const color = 0x192348
+  const near = 4;
+  const far = 80;
+  scene.fog = new THREE.Fog(color, near, far);
+  scene.background = new THREE.Color(color);
+}
+
 
 const loader = new GLTFLoader();
 const textureLoader = new THREE.TextureLoader();
-const emissionTexture = textureLoader.load('/assets/motherboard_emit.jpg');
-
+const emissionMap = textureLoader.load('./assets/motherboard_emit.jpg');
 let mixer;
 
-loader.load( 'portfolio3.glb', function ( glb ) {
+loader.load( 'k.glb', function ( glb ) {
 
 	scene.add( glb.scene );
 
   glb.scene.traverse((child) => {
-    if (child.isMesh) {
+    if (child.isMesh && child.name === "motherboard") { // Apply only to "motherboard"
       const material = new THREE.MeshStandardMaterial({
-        color: child.material.color || 0xaaaaaa, 
-        emissive: child.material.emissive || 0x000000,  
-        emissiveIntensity: 1, 
+        color: child.material.color || 0xe79ddf, 
+        emissive: child.material.emissive || 0xe79ddf,  
+        emissiveIntensity: 0.05, 
+        emissiveMap: emissionMap,
+        
       });
 
       child.material = material;
@@ -51,11 +58,16 @@ loader.load( 'portfolio3.glb', function ( glb ) {
   mixer = new THREE.AnimationMixer(glb.scene);
   glb.animations.forEach(clip => {
     const action = mixer.clipAction(clip);
-    action.setLoop(THREE.LoopOnce);  // Set loop mode to 'once'
-    action.clampWhenFinished = true; // Ensure the animation stays on the last frame when finished
+    action.setLoop(THREE.LoopOnce);  
+    action.clampWhenFinished = true; 
     action.play();
   });
-
+  action.addEventListener("finished", () =>
+  {
+    action.time = 50;
+    action.setLoop(THREE.LoopRepeat, Infinity);
+    action.play();
+  });
 }, undefined, function ( error ) {
 
 	console.error( error );
@@ -70,20 +82,21 @@ directionalLight.castShadow = true;
 scene.add( directionalLight );
 
 
-// Camera position and rotation trackball
-camera.position.set(-17.461, 15.847, -20.062);
+// Camera position and rotation 
+camera.position.set(-17.661, 14.607, -20.982);
 camera.rotation.set(
   THREE.MathUtils.degToRad(-144.01),  
   THREE.MathUtils.degToRad(-34.93),   
   THREE.MathUtils.degToRad(-157.42)   
 );
-camera.fov = 22.90;
+camera.fov = 22.50;
 camera.updateProjectionMatrix();
 
 
-// Controls
+// // Controls
 const controls = new OrbitControls( camera, canvas);
 controls.enablePan = false; 
+controls.enableRotate = true;
 controls.minDistance = 5;  
 controls.maxDistance = 50;  
 controls.minPolarAngle = THREE.MathUtils.degToRad(10);
@@ -106,11 +119,9 @@ window.addEventListener("resize", handleResize);
 function animate() {
   requestAnimationFrame(animate);
 
-  
   if (mixer) {
     mixer.update(0.01);  
   }
-
   controls.update(); 
   renderer.render(scene, camera); 
 }
