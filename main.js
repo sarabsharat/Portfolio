1e0b76acc34518a6221e28b4a9563c2dc5e3e161
sarabsharat <sarabsharat.sb@gmail.com>
2025-02-25 23:12:12 +0300

    idk

--- main.js
+++ main.js
@@ -37,7 +37,15 @@ const mouse = new THREE.Vector2();
 const loader = new GLTFLoader();
 
 let hoveredObject = null;
-let mixer;
+let mixer, cameraMixer;
+const clock = new THREE.Clock();
+const previousCameraPosition = new THREE.Vector3();
+const previousCameraQuaternion = new THREE.Quaternion();
+let isCameraAnimating = false;
+let animatedCamera = null;
+let clips = [];
+let cameraClips = [];
+
 
 const intersectObjects = [];
 const intersectObjectsNames =[
@@ -51,53 +59,78 @@ const intersectObjectsNames =[
 ];
 
 
-const cameraTargets = {
-  Cresume: {
-    position: new THREE.Vector3(0.5196626782417297, 0.1496729850769043, -5.489231109619141),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-  },
-  Cexperience:{
-    position: new THREE.Vector3(-5.572940826416016, 0.1496729850769043, 0.5330694913864136),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-  },
-  Cskills:{
-    position: new THREE.Vector3(-5.461484909057617, 0.1496729850769043, 6.5523552894592285),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-  },
-  Ctestimonials:{
-    position: new THREE.Vector3(6.557930946350098, 0.15647836029529572, -5.484166145324707),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-    },
-  Cwork:{
-    position: new THREE.Vector3(0.5451653003692627, 0.1496729850769043, 6.543992042541504),
-    rotation: new THREE.Euler(0,0,0),
-    duration: 1.5
-  },
-  Cabuot_me:{
-    position: new THREE.Vector3(6.557510852813721, 0.1496729850769043, 0.5195545554161072),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-  },
-  Ccontact_me:{
-    position: new THREE.Vector3(6.554821968078613, 0.1496729850769043, 6.461586952209473),
-    rotation: new THREE.Euler(0, 0, 0),
-    duration: 1.5
-  }
-};
+// const cameraTargets = {
+//   Cresume: {
+//     position: new THREE.Vector3(0.5196626782417297, 0.1496729850769043, -5.489231109619141),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//   },
+//   Cexperience:{
+//     position: new THREE.Vector3(-5.572940826416016, 0.1496729850769043, 0.5330694913864136),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//   },
+//   Cskills:{
+//     position: new THREE.Vector3(-5.461484909057617, 0.1496729850769043, 6.5523552894592285),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//   },
+//   Ctestimonials:{
+//     position: new THREE.Vector3(6.557930946350098, 0.15647836029529572, -5.484166145324707),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//     },
+//   Cwork:{
+//     position: new THREE.Vector3(0.5451653003692627, 0.1496729850769043, 6.543992042541504),
+//     rotation: new THREE.Euler(0,0,0),
+//     duration: 1.5
+//   },
+//   Cabuot_me:{
+//     position: new THREE.Vector3(6.557510852813721, 0.1496729850769043, 0.5195545554161072),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//   },
+//   Ccontact_me:{
+//     position: new THREE.Vector3(6.554821968078613, 0.1496729850769043, 6.461586952209473),
+//     rotation: new THREE.Euler(0, 0, 0),
+//     duration: 1.5
+//   }
+// };
 
 
 const originalMaterials = new Map();
 const clonedMaterials = new Map();
+const objectAnimationMap = {
+  Cresume:'cameraResume',
+  Cexperience:'cameraExperience',
+  Cskills:'cameraSkills',
+  Ctestimonials:'cameraTestimonials',
+  Cwork:'cameraWork',
+  Cabuot_me:'camerAboutme',
+  Ccontact_me:'cameraContactme',
+};
+// const objectAnimationMap = {
+//   Cresume:3,
+//   Cexperience:2,
+//   Cskills:4,
+//   Ctestimonials:5,
+//   Cwork:6,
+//   Cabuot_me:0,
+//   Ccontact_me:1,
+// };
 
 loader.load( '192.glb', function ( glb ) {
 
 	scene.add( glb.scene );
-
+  const clips = glb.animations;
+  cameraClips = clips.filter(c => c.name.includes('camer'));
+  
+  console.log('Actual animation names:', glb.animations.map(a => a.name));
   glb.scene.traverse((child) => {
+  mixer = new THREE.AnimationMixer(glb.scene);
+  if (child.isCamera) animatedCamera = child;
+
+
     if (intersectObjectsNames.includes(child.name)) {
       intersectObjects.push(child);
 
@@ -116,73 +149,48 @@ loader.load( '192.glb', function ( glb ) {
         clonedMaterials.set(child, clonedMaterial);
       }}
 
-  });
-
-
-// trauma
-//   const textureLoader = new THREE.TextureLoader();
-//   const emissionMap = textureLoader.load('./assets/motherboard_emit.jpg');
-
-//   if (child.isMesh && child.name === "motherboard") { 
-//     const material = new THREE.MeshStandardMaterial({
-//       color: child.material.color || 0xe79ddf, 
-//       emissive: child.material.emissive || 0xe79ddf,  
-//       emissiveIntensity: 0.05, 
-//       emissiveMap: emissionMap,
-      
-//     });
-//   }
-
-  
-
-
 //Animation
-  const objectAnimationMap = {
-    Cresume:3,
-    Cexperience:2,
-    Cskills:4,
-    Ctestimonials:5,
-    Cwork:6,
-    Cabuot_me:0,
-    Ccontact_me:1,
-  };
-  const clips = glb.animations;
-  const start = [clips[7],clips[8],
-                 clips[10], clips[11],
-                 clips[13], clips[15],
-                 clips[16],clips[18],
-                 clips[19]
-                 ];
-                
-  const loop = [clips[9], clips[12], clips[14],clips[17]];
-  mixer = new THREE.AnimationMixer(glb.scene);
-  console.log(clips);
-  let lastStartAction = null;
-    
-    start.forEach((clip, index) => {
-    const action = mixer.clipAction(clip);
-    action.setLoop(THREE.LoopOnce);
-    action.clampWhenFinished = true;
-    action.play();
- 
-    if (index === start.length - 1) {
-      lastStartAction = action; 
-    }
-   });
- 
- 
-  if (lastStartAction) {
-    lastStartAction.getMixer().addEventListener("finished", () => {
-       loop.forEach(clip => {
-         const action = mixer.clipAction(clip);
-        action.time = 50; 
-        action.setLoop(THREE.LoopRepeat, Infinity);
-        action.play();
-      });
-    });
-    } 
+
+
+const start = [clips[7],clips[8],
+clips[10], clips[11],
+clips[13], clips[15],
+clips[16],clips[18],
+clips[19]
+];
+
+const loop = [clips[9], clips[12], clips[14],clips[17]];
+
+
+let lastStartAction = null;
+
+start.forEach((clip, index) => {
+const action = mixer.clipAction(clip);
+action.setLoop(THREE.LoopOnce);
+action.clampWhenFinished = true;
+action.play();
+
+if (index === start.length - 1) {
+lastStartAction = action; 
+}
+});
+
+
+if (lastStartAction) {
+lastStartAction.getMixer().addEventListener("finished", () => {
+loop.forEach(clip => {
+const action = mixer.clipAction(clip);
+action.time = 50; 
+action.setLoop(THREE.LoopRepeat, Infinity);
+action.play();
+});
+});
+} 
+});
+
+
   });
-  
+
 
 
 //Light
@@ -193,7 +201,6 @@ scene.add( directionalLight );
 
 
 //Camera position and rotation 
-
 camera.position.set(-17.941, 17.759, -18.720);
 camera.rotation.set(
   THREE.MathUtils.degToRad(-135.84),  
@@ -203,32 +210,27 @@ camera.rotation.set(
 camera.fov = 25.64;
 const camerainit = camera.position.clone();
 camera.updateProjectionMatrix();
+
 let mouseX = 0;
 let mouseY = 0;
 
-function playAnimation(animationIndex) {
-  if (!clips || animationIndex >= clips.length) {
-    console.error('Invalid animation index:', animationIndex);
-    return;
-  }
 
-  
-  mixer.stopAllAction();
-
-  
-  const clip = clips[animationIndex];
-  const action = mixer.clipAction(clip);
-
-  action.setLoop(THREE.LoopOnce); 
-  action.clampWhenFinished = true; 
-  action.play();
-
-  
-  action.getMixer().addEventListener('finished', () => {
-    console.log('Animation finished:', clip.name);
+const xButton = document.getElementById('XButton');
+const Resume = document.getElementById('Resume');
+if (xButton) {
+  xButton.addEventListener('click', () => {
+    xButton.onclick = function(){
+      Resume.style.display = "none";
+      console.log("tf");
+    }
+    if (isCameraAnimating) return;
+    camera.position.copy(previousCameraPosition);
+    camera.quaternion.copy(previousCameraQuaternion);
+    
+    console.log("exit");
   });
 }
-
+ 
 
 
 //Responsivness
@@ -284,15 +286,11 @@ function onMouseMove( event ) {
 function onClick(){
   raycaster.setFromCamera(mouse, camera);
   const intersects = raycaster.intersectObjects(intersectObjects);
-  if(intersects.length > 0){
+  if (intersects.length > 0) {
     const objectName = intersects[0].object.name;
-
-    if (objectAnimationMap[objectName] !== undefined) {
-      const animationIndex = objectAnimationMap[objectName];
-      playAnimation(animationIndex);
-    }
+    console.log("object clicked",objectName);
+    
   }
-  
 }
 
 window.addEventListener("resize", onResize);
@@ -311,7 +309,7 @@ function mobileControls (){
   camera.position.set(-17.941, 17.759, -18.720);
   controls.target.set(0,0,0);
   controls.enableDamping = true;
-  controls.enablezoom = true;
+  controls.enableZoom = true;
   controls.dampingFactor = 0.05;
   controls.screenSpacePanning = true;
   controls.maxDistance = 50;
@@ -330,9 +328,12 @@ if (debugMobile) isMobile = true;
 mobileControls();
 
 
+
 // Animation
 function animate() {
   requestAnimationFrame(animate);
+  
+
   if (isMobile && controls){
       controls.update();
     } else {
